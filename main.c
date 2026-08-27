e83e205af444c5489441441b94e4c1a12d1f6daf
Taha-breizi <tahabreizi@gmail.com>
2026-08-27 23:30:30 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,1 @@
 
-char website_to_delete[100] = "";
-printf("\nEnter website name to delete: ");
-fgets(website_to_delete, sizeof(website_to_delete), stdin);
-website_to_delete[strcspn(website_to_delete, "\n")] = '\0';char website_to_delete[100] = "";
-printf("\nEnter website name to delete: ");
-fgets(website_to_delete, sizeof(website_to_delete), stdin);
-website_to_delete[strcspn(website_to_delete, "\n")] = '\0';char website_to_delete[100] = "";
-printf("\nEnter website name to delete: ");
-fgets(website_to_delete, sizeof(website_to_delete), stdin);
-website_to_delete[strcspn(website_to_delete, "\n")] = '\0';char website_to_delete[100] = "";
-printf("\nEnter website name to delete: ");
-fgets(website_to_delete, sizeof(website_to_delete), stdin);
-website_to_delete[strcspn(website_to_delete, "\n")] = '\0';char website_to_delete[100] = "";
-printf("\nEnter website name to delete: ");
-fgets(website_to_delete, sizeof(website_to_delete), stdin);
-website_to_delete[strcspn(website_to_delete, "\n")] = '\0';
